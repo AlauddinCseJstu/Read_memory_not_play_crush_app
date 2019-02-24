051c854f54395739b0b46ac9d71688fb6163ce54
Alauddin <alauddin.cse.2019@gmail.com>
2019-02-24 07:46:13 -0800

    Initial commit

---
+++ Song.java
@@ -0,0 +1,22 @@
+package com.example.alauddin.androidmedia;
+
+/**
+ * Created by Alauddin on 2/18/2019.
+ */
+
+public class Song {
+    private long id;
+    private String title;
+    private String artist;
+
+    public Song(long songID, String songTitle, String songArtist) {
+        id=songID;
+        title=songTitle;
+        artist=songArtist;
+    }
+    public long getID(){return id;}
+    public String getTitle(){return title;}
+    public String getArtist(){return artist;}
+
+
+}
